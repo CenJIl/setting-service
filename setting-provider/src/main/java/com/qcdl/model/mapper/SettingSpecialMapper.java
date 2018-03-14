82953d07a940a7899e10f88556878e420581e122
tuwenyu <tuwenyu1234>
2018-03-14 15:28:16 +0800

    专题管理

--- setting-provider/src/main/java/com/qcdl/model/mapper/SettingSpecialMapper.java
+++ setting-provider/src/main/java/com/qcdl/model/mapper/SettingSpecialMapper.java
@@ -1,8 +1,13 @@
 package com.qcdl.model.mapper;
 
 import com.qcdl.model.entity.SettingSpecial;
+import com.qcdl.rest.param.SpecialParam;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
+import java.util.List;
+
 public interface SettingSpecialMapper extends Mapper<SettingSpecial>, MySqlMapper<SettingSpecial> {
+
+    List<SpecialParam> specialList(SpecialParam specialParam);
 }
